a404776474fc31e8221cc1e475b56c1a32c20230
Nithin1729S <sureshnithin1729@gmail.com>
2025-03-09 12:47:01 +0530

    update metadata

--- frontend/next-seo.config.js
+++ frontend/next-seo.config.js
@@ -3,14 +3,14 @@ const defaultSEOConfig = {
   titleTemplate: '%s | ಅಕ್ಷರ ಕಲಿ',
   defaultTitle: 'ಅಕ್ಷರ ಕಲಿ',
   description: 'The #1 educational app for kids to learn the Kannada Alphabets in a funtastic way.',
-  canonical: 'https://HCI.vercel.app',
+  canonical: 'https://kannada-abc.vercel.app',
   openGraph: {
-    url: 'https://xxxx.vercel.app',
-    title: 'HCI',
+    url: 'https://kannada-abc.vercel.app',
+    title: 'ಅಕ್ಷರ ಕಲಿ',
     description: '#1 Animal ABCs app for kids to learn the Kannada Alphabets.',
     images: [
       {
-        url: 'https://xxxx.vercel.app/static/og-image.png',
+        url: 'https://kannada-abc.vercel.app/opengraph.png',
         alt: 'ಅಕ್ಷರ ಕಲಿ',
       },
     ],
