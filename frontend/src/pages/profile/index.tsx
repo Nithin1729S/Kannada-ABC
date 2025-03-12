17520bb06b35f5ba2606775cd514dbe4f03b0f06
Nithin1729S <sureshnithin1729@gmail.com>
2025-03-12 21:37:41 +0530

    my profile page design

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -13,12 +13,11 @@ import { useScrollReveal } from '~/src/hooks/useScrollReveal'
 
 import ImgPlay from '~public/img/bg-play.svg'
 import ImgGameSwap from '~public/img/bg-game-swap.svg'
-import { useSession } from "next-auth/react";
-
+import { useSession } from 'next-auth/react'
 
 export default function Play() {
   const { scrollReveal } = useScrollReveal({ offset: ['start end', 'end start'] })
-  const { data: session } = useSession();
+  const { data: session } = useSession()
   return (
     <Box layerStyle="page" pos="relative" zIndex={1} px={0} pb={0}>
       <Fade transition={{ enter: { duration: 0.6 } }} in>
@@ -29,79 +28,23 @@ export default function Play() {
           Profile
         </Heading>
       </Fade>
-      <Box mt={20} mb={24}>
+      <Box
+        transform="scale(0.4)"
+        transformOrigin="top center"
+        height="60px" // Reduced height to minimize space
+        mb="120px" // Negative margin to pull next content up
+      >
         <BannerHeads />
       </Box>
-      {session && <Text>Signed in as {session.user?.email}</Text>}
 
-      <SlideFade transition={{ enter: { duration: 0.6, delay: 0.25 + 0.65 + 2.25 } }} in>
-        <MotionBox
-          minH="sm"
-          mx={[1, 3]}
-          bg="blackAlpha.500"
-          pb={16}
-          roundedTop={['card', 'bigCard']}
-          {...scrollReveal}
-        >
-          <Box px={4} py={12} color="text.inverse" textAlign="center">
-            <Heading maxW={[44, null, 'none']} mx="auto" mb={4} lineHeight="none" variant="body">
-              <Box as="span" color="text.highlight" fontSize="2xl">
-                49
-              </Box>{' '}
-              Letters Mastery Level
-            </Heading>
-            <Text opacity={0.75}>Level Up in your Quest.</Text>
-          </Box>
-          <Underline mb={12} fill="brand.900" />
-          <List sx={{ '--size': 'sizes.60' }} layerStyle="gridy" px={[5, 12]}>
-            <MotionPop as="li" marge="0px" once>
-              <Flex h="full" maxH={[64, null, 72, null, 'none']} rounded="card">
-                <GameCard title="Letter 1" colorScheme="orange" />
-              </Flex>
-            </MotionPop>
-            <MotionPop as="li" marge="0px" once>
-              <Flex h="full" maxH={[64, null, 72, null, 'none']} rounded="card">
-                <GameCard title="Letter 2" colorScheme="pink" />
-              </Flex>
-            </MotionPop>
-            <MotionPop as="li" marge="0px" once>
-              <Flex h="full" maxH={[64, null, 72, null, 'none']} rounded="card">
-                <GameCard imgSrc={ImgGameSwap} title="Letter 3" colorScheme="blue" />
-              </Flex>
-            </MotionPop>
-            <MotionPop as="li" marge="0px" once>
-              <Flex h="full" maxH={[64, null, 72, null, 'none']} rounded="card">
-                <GameCard title="Letter 4" colorScheme="cyan" />
-              </Flex>
-            </MotionPop>
-            <MotionPop as="li" marge="0px" once>
-              <Flex h="full" maxH={[64, null, 72, null, 'none']} rounded="card">
-                <GameCard imgSrc={ImgGameSwap} title="Letter 5" />
-              </Flex>
-            </MotionPop>
-            <MotionPop as="li" marge="0px" once>
-              <Flex h="full" maxH={[64, null, 72, null, 'none']} rounded="card">
-                <GameCard colorScheme="yellow" title='Letter 6' />
-              </Flex>
-            </MotionPop>
-            <MotionPop as="li" marge="0px" once>
-              <Flex h="full" maxH={[64, null, 72, null, 'none']} rounded="card">
-                <GameCard
-                  href="/play"
-                  imgSrc={ImgGameSwap}
-                  title="Letter 7"
-                  colorScheme="red"
-                />
-              </Flex>
-            </MotionPop>
-            <MotionPop as="li" marge="0px" once>
-              <Flex h="full" maxH={[64, null, 72, null, 'none']} rounded="card">
-                <GameCard title="Letter 8" colorScheme="purple" />
-              </Flex>
-            </MotionPop>
-          </List>
-        </MotionBox>
-      </SlideFade>
+      <Box px={4} py={12} color="text.inverse" textAlign="center">
+        <Heading maxW={[44, null, 'none']} mx="auto" mb={4} lineHeight="none" variant="body">
+          <Box as="span" color="text.highlight" fontSize="2xl"></Box>{' '}
+          {session && <Text>Signed in as {session.user?.email}</Text>}
+        </Heading>
+        <Text opacity={0.75}>Level Up in your Quest.</Text>
+      </Box>
+      <Underline mb={12} fill="brand.900" />
     </Box>
   )
 }
