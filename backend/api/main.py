e729da8803724e38f16c448e96fe9ab8a443bc1f
Nithin1729S <sureshnithin1729@gmail.com>
2025-03-15 18:54:54 +0530

    allowed all origins for now

--- backend/api/main.py
+++ backend/api/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Allow requests from the frontend running at localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
